5b9729f58ba61b1d8baaaf5a2d452cd24f93e90f
Samantha Lau <samantha@coingecko.com>
2021-06-27 15:09:23 +0800

    Add form and submit button, add header and Gratifuel title

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,20 @@ import "./App.css";
 
 function App() {
   return (
-    <main className="App">
-      <h1 className="heading">Hi name.</h1>
-      <p>What are you grateful for today?</p>
+    <div className="App">
+      <header>
+        <h1 className="heading">Gratifuel</h1>
+      </header>
+      <main>
+        <h1 className="heading">Hi name.</h1>
+        <p>What are you grateful for today?</p>
 
-      <input type="text" placeholder="I am doing okay" />
-    </main>
+        <form>
+          <input type="text" placeholder="I am doing okay" />
+          <button className="submit-btn">I am grateful!</button>
+        </form>
+      </main>
+    </div>
   );
 }
 
