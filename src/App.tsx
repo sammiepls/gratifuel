2b81909a43f43ec5c4ade5c7617174485b00d8e3
sammiepls <samantha.lhe@gmail.com>
2021-07-08 22:09:03 +0800

    Add permissions to manifest and create basic storage of gratifuels

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "./App.css";
 
 function App() {
+  const [gratifuels, setGratifuels] = useState<string[]>([]);
+  const [input, setInput] = useState("");
+
+  const getGratifuels = () => {
+    chrome.storage.sync.get("gratifuels", (data) => {
+      if (data.gratifuels) {
+        setGratifuels(data.gratifuels);
+      }
+    });
+  };
+
+  const storeGratifuels = useCallback(() => {
+    chrome.storage.sync.set({ gratifuels: [...gratifuels, input] });
+  }, [input, gratifuels]);
+
+  const clearGratifuels = () => {
+    chrome.storage.sync.clear();
+    setGratifuels([]);
+  };
+
+  useEffect(() => {
+    getGratifuels();
+  }, []);
+
+  useEffect(() => {
+    storeGratifuels();
+  }, [gratifuels, storeGratifuels]);
+
+  const handleInputChange = (e: React.FormEvent<HTMLInputElement>) =>
+    setInput(e.currentTarget.value);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (input !== "") {
+      setGratifuels((state) => [...state, input]);
+      setInput("");
+    }
+  };
+
   return (
     <div className="App">
       <header>
@@ -10,11 +49,22 @@ function App() {
       <main>
         <h1 className="heading">Hi name.</h1>
         <p>What are you grateful for today?</p>
+        <button onClick={clearGratifuels}>Clear gratifuels</button>
 
-        <form>
-          <input type="text" placeholder="I am doing okay" />
+        <form onSubmit={handleSubmit}>
+          <input
+            value={input}
+            onChange={handleInputChange}
+            type="text"
+            placeholder="I am doing okay"
+          />
           <button className="submit-btn">I am grateful!</button>
         </form>
+        <ul>
+          {gratifuels.map((g) => (
+            <li>{g}</li>
+          ))}
+        </ul>
       </main>
     </div>
   );
